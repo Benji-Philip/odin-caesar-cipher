163565ab122cf8651e8616fbef96c522bf217a7b
Benji-Philip <99130709+Benji-Philip@users.noreply.github.com>
2024-07-19 22:01:09 +0530

    my solution to creating the caesar cipher

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -4,18 +4,38 @@ string = gets.chomp
 number_is_valid = false
 
 until number_is_valid
-  puts "Shift by : (default = 4)"
-  number = gets.chomp.to_i
-  number_is_valid = number != 0 # what if the number is too big
+  puts "Shift by :"
+  shift_number = gets.chomp.to_i
+  number_is_valid = shift_number != 0 # what if the number is too big
   if number_is_valid
-    puts "Shifting by #{number}"
+    puts "Shifting by #{shift_number}"
   else
     puts "Invalid input"
   end
 end
 
 def caesar_cipher(string_to_encrypt, shift)
-  string_to_encrypt.chars.map do |character|
-    character.ord
+  encrypted_array = string_to_encrypt.bytes.map do |character|
+    if character.between?(65, 91)
+      caesar_calculator(65, 90, character, shift).chr
+    elsif character.between?(97, 123)
+      caesar_calculator(97, 122, character, shift).chr
+      # each character get turned into numbers and shift is added to it but within limits and having it round of
+    else
+      character.chr
+    end
+  end
+  encrypted_array.join
+end
+
+def caesar_calculator(lower_limit, upper_limit, number_to_check, shift)
+  if number_to_check + shift > upper_limit
+    return lower_limit + ((number_to_check + shift) - upper_limit)
+  elsif number_to_check + shift < lower_limit
+    return upper_limit - (lower_limit - (number_to_check + shift))
+  else
+    return number_to_check + shift
   end
 end
+
+p caesar_cipher(string, shift_number)
